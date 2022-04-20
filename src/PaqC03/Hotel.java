01aca1547b94a5191e812a3adf9a011cc01b3f5c
DaniTurMayor <daniel.tur@alu.uclm.es>
2022-04-20 23:23:57 +0200

    Version 2.0

--- src/PaqC03/Hotel.java
+++ src/PaqC03/Hotel.java
@@ -10,74 +10,111 @@ public class Hotel implements Serializable {
         hotel = new Cliente[8][6];
     }
 
+
+
     public int reservarHab(Cliente cl, String opc, int cantHab) {
         int fila = 0;
         int columna = 0;
-        int reservas=0;
+        int reservas = 0;
         switch (opc) {
             case "Estandar":
-                for (int n = 1; n <= cantHab; n++) {
+                for (reservas = 0 ; reservas < cantHab;) {
                     for (int i = 0; i < 5; i++) {
                         for (int j = 0; j < 6; j++) {
                             if (hotel[i][j] == null) {
-                                if (cantHab>0){
-                                    hotel[i][j] = cl;
-                                    cantHab--;
-                                    reservas++;
-                                }
+                                hotel[i][j] = cl;
+                                reservas++;
+                                if(reservas == cantHab) return reservas;
                             }
                         }
                     }
                 }
                 return reservas;
+
             case "Balcon":
-                for (int n = 0; n < cantHab; n++) {
+                for (reservas = 0 ; reservas < cantHab;) {
                     for (int i = 5; i < 7; i++) {
                         for (int j = 0; j < 6; j++) {
                             if (hotel[i][j] == null) {
-                                if (cantHab>0){
-                                    hotel[i][j] = cl;
-                                    cantHab--;
+                                hotel[i][j] = cl;
+                                reservas++;
+                                if(reservas == cantHab) return reservas;
+                            }
+                        }
+                    }
+                }
+
+            case "Suite":
+                fila = 7;
+                for (reservas = 0 ; reservas < cantHab;) {
+                    for (int j = 0; j < 6; j++) {
+                        if (hotel[fila][j] == null) {
+                            hotel[fila][j] = cl;
+                            reservas++;
+                            if (reservas == cantHab) return reservas;
+                        }
+                    }
+                }
+        }
+        return reservas;
+    }
+
+
+
+    public boolean anularReserva(Cliente cl, String opc, int cantHab) {
+        int fila = 0;
+        int columna = 0;
+        int reservas = 0;
+        switch (opc) {
+            case "Estandar":
+                for (reservas = 0 ; reservas < cantHab;) {
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < 6; j++) {
+                            if (hotel[i][j] != null) {
+                                if (hotel[i][j].equals(cl)) {
+                                    hotel[i][j] = null;
                                     reservas++;
+                                    if (reservas == cantHab) return true;
                                 }
                             }
                         }
                     }
                 }
-                return reservas;
-            case "Suite":
-                fila=7;
-                for (int n = 0; n < cantHab; n++) {
-                    for (int j = 0; j < 6; j++) {
-                        if (hotel[fila][j] == null) {
-                            if (cantHab>0){
-                                hotel[fila][j] = cl;
-                                cantHab--;
-                                reservas++;
+                if (reservas == cantHab) return true;
+
+
+            case "Balcon":
+                for (reservas = 0 ; reservas < cantHab;) {
+                    for (int i = 5; i < 7; i++) {
+                        for (int j = 0; j < 6; j++) {
+                            if (hotel[i][j] != null) {
+                                if (hotel[i][j].equals(cl)) {
+                                    hotel[i][j] = null;
+                                    reservas++;
+                                    if (reservas == cantHab) return true;
+                                }
                             }
                         }
                     }
                 }
-                return reservas;
-            }
-        return reservas;
-    }
 
-    public boolean anularReserva(Cliente cl, int cantHab) {
-        for (int n = 0; n < cantHab; n++) {
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 6; j++) {
-                    if (hotel[i][j] != null) {
-                        if (hotel[i][j].equals(cl)) {
-                            hotel[i][j] = null;
-                            return true;
+            case "Suite":
+                fila = 7;
+                for (reservas = 0 ; reservas < cantHab;) {
+                    for (int j = 0; j < 6; j++) {
+                        if (hotel[fila][j] != null) {
+                            if (hotel[fila][j].equals(cl)) {
+                                hotel[fila][j] = null;
+                                reservas++;
+                                if (reservas == cantHab) return true;
+                            }
                         }
                     }
                 }
-            }
         }
-        return false;
+        return true;
     }
+
 
     public String toString(){
         String disponible="";
